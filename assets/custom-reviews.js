508315f33956cb81f3a6754ac8d85eb7e0745af3
mudmykola <mykola.mud@gmail.com>
2024-02-13 10:41:39 +0200

    add reviews slider block

---
+++ assets/custom-reviews.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const slidesContainer = document.querySelector('.slides-container');
+    const prevBtn = document.querySelector('.prev-btn');
+    const nextBtn = document.querySelector('.next-btn');
+
+    let slideIndex = 0;
+    const numSlides = document.querySelectorAll('.slide').length;
+    const slideWidth = document.querySelector('.slide').offsetWidth;
+    const numVisibleSlides = 4; // Кількість видимих карточок
+
+    function scrollToSlide(index) {
+        slidesContainer.style.transform = `translateX(-${index * slideWidth}px)`;
+        slideIndex = index;
+    }
+
+    function prevSlide() {
+        if (slideIndex > 0) {
+            slideIndex--;
+            scrollToSlide(slideIndex);
+        }
+    }
+
+    function nextSlide() {
+        if (slideIndex < numSlides - numVisibleSlides) {
+            slideIndex++;
+            scrollToSlide(slideIndex);
+        }
+    }
+
+    prevBtn.addEventListener('click', prevSlide);
+    nextBtn.addEventListener('click', nextSlide);
+});
